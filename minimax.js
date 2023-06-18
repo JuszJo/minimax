3df68489106c422ad7d54c34f066812f4bb90e87
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-06-18 20:31:07 +0100

    add functions to determin square click

--- minimax.js
+++ minimax.js
@@ -1,3 +1,14 @@
+const board = document.getElementById('grid');
+const squares = document.getElementsByClassName('square');
+
+const squaresArray = Array.from(squares);
+
+squaresArray.forEach(square => square.addEventListener('click', handleSquareClick))
+
+function handleSquareClick() {
+    console.log(this);
+}
+
 function minimax(board, player) {
     // code goes here
 }
