cbb007e8230f60cf7c41719a690483896da704a4
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-06-18 20:40:57 +0100

    add x and o functionality

--- minimax.js
+++ minimax.js
@@ -1,12 +1,22 @@
 const board = document.getElementById('grid');
 const squares = document.getElementsByClassName('square');
+const moves = ['X', 'O'];
+
 
 const squaresArray = Array.from(squares);
 
 squaresArray.forEach(square => square.addEventListener('click', handleSquareClick))
 
 function handleSquareClick() {
-    console.log(this);
+    const move = moves.splice(0, 1);
+
+    this.innerHTML = `
+        <div class="move-div">   
+            <h2>${move}</h2>
+        </div>
+    `;
+
+    moves.push(move);
 }
 
 function minimax(board, player) {
